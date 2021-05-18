597c8432176e98ec30b926d95b4884283b464a40
Choi <roundbeargames@gmail.com>
2021-05-19 01:32:37 +0900

    don't need unitsprite

--- RunnerProject_2020_3/Assets/InfiniteRunnerProject/Unit/Unit.cs
+++ RunnerProject_2020_3/Assets/InfiniteRunnerProject/Unit/Unit.cs
@@ -8,7 +8,6 @@ namespace RB
     {
         public StateController stateController = null;
         public UnitData unitData = null;
-        public UnitSprite unitSprite = null;
         public CollisionDetector collisionDetector = null;
 
         public List<SpriteAnimation> listSpriteAnimations = new List<SpriteAnimation>();
@@ -38,32 +37,6 @@ namespace RB
                     spriteAni.gameObject.SetActive(false);
                 }
             }
-        }
-
-        public virtual void AttachSprite(UnitSprite sprite, Vector2 pixelSize, OffsetType offsetType)
-        {
-            unitSprite = sprite;
-            
-            if (unitSprite.spriteRenderer == null)
-            {
-                unitSprite.spriteRenderer = unitSprite.gameObject.GetComponentInChildren<SpriteRenderer>();
-            }
-
-            float xScale = pixelSize.x / unitSprite.spriteRenderer.sprite.bounds.size.x;
-            float yScale = pixelSize.y / unitSprite.spriteRenderer.sprite.bounds.size.y;
-
-            unitSprite.spriteRenderer.transform.localScale = new Vector2(xScale, yScale);
-
-            unitSprite.gameObject.transform.parent = this.transform;
-            unitSprite.gameObject.transform.localPosition = Vector3.zero;
-            unitSprite.gameObject.transform.localRotation = Quaternion.identity;
-
-            if (offsetType == OffsetType.BOTTOM_CENTER)
-            {
-                unitSprite.spriteRenderer.transform.localPosition = new Vector3(0f, unitSprite.spriteRenderer.sprite.bounds.size.y * yScale * 0.5f, 0f);
-            }
-
-            Debugger.Log("sprite attached: " + unitSprite.gameObject.name + " " + unitSprite.spriteRenderer.size);
         }
 
         public virtual void SetParent(Transform ownerTransform)
