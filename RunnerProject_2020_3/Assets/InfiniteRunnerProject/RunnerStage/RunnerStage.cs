724c4b435be3150ea70f70588c5d96bcddd11f37
Choi <roundbeargames@gmail.com>
2021-07-01 00:49:26 +0900

    instantiating runner in runnerstage

--- RunnerProject_2020_3/Assets/InfiniteRunnerProject/RunnerStage/RunnerStage.cs
+++ RunnerProject_2020_3/Assets/InfiniteRunnerProject/RunnerStage/RunnerStage.cs
@@ -6,24 +6,31 @@ namespace RB
 {
     public class RunnerStage : Stage
     {
+        private UserInput _userInput = new UserInput();
+
         public override void Init()
         {
+            units.AddCreator(new Runner_Creator(_userInput, this.transform));
+            units.ProcessCreators();
 
+            //runner must already be in the list
+            units.AddCreator(new CameraController_Creator(this.transform, units.GetUnit<Runner>(), FindObjectOfType<Camera>()));
+            units.ProcessCreators();
         }
 
         public override void OnUpdate()
         {
-
+            _userInput.OnUpdate();
         }
 
         public override void OnFixedUpdate()
         {
-
+            units.OnFixedUpdate();
         }
 
         public override void OnLateUpdate()
         {
-
+            units.OnLateUpdate();
         }
     }
 }
