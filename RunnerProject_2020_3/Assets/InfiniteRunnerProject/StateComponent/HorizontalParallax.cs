5307a21bc3af75c385be1126998b72eb8c2bb82b
Choi <roundbeargames@gmail.com>
2021-07-08 12:38:39 +0900

    parallax startpos x & y

--- RunnerProject_2020_3/Assets/InfiniteRunnerProject/StateComponent/HorizontalParallax.cs
+++ RunnerProject_2020_3/Assets/InfiniteRunnerProject/StateComponent/HorizontalParallax.cs
@@ -7,12 +7,15 @@ namespace RB
     public class HorizontalParallax : StateComponent
     {
         float _yPositionFix = 0f;
+        float _xStartingPos = 0f;
+
         GameObject _parallaxAnchor = null;
         float _percentage = 0f;
        
         public HorizontalParallax(Unit unit, GameObject anchor, float percentage)
         {
             _unit = unit;
+            _xStartingPos = unit.transform.position.x;
             _yPositionFix = unit.transform.position.y;
             _parallaxAnchor = anchor;
             _percentage = percentage;
@@ -23,7 +26,7 @@ namespace RB
             if (_parallaxAnchor != null)
             {
                 Vector3 pos = _parallaxAnchor.transform.position * _percentage;
-                _unit.transform.position = new Vector3(pos.x, _yPositionFix, _unit.transform.position.z);
+                _unit.transform.position = new Vector3(_xStartingPos + pos.x, _yPositionFix, _unit.transform.position.z);
             }
         }
     }
