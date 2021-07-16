54c50432b14550e7d845cbcfdb5610d72fbff196
RBG <roundbeargames@gmail.com>
2021-07-17 01:35:07 +0900

    golemattack

--- RunnerProject_2020_3/Assets/InfiniteRunnerProject/GolemEnemy/Golem_Attack.cs
+++ RunnerProject_2020_3/Assets/InfiniteRunnerProject/GolemEnemy/Golem_Attack.cs
@@ -11,11 +11,17 @@ namespace RB
         public Golem_Attack(Unit unit)
         {
             _unit = unit;
+            _listStateComponents.Add(new OverlapBoxCollision(_unit, GameInitializer.current.golem_Attack_OverlapBoxSO.listSpecs));
         }
 
         public override void OnFixedUpdate()
         {
+            FixedUpdateComponents();
 
+            if (_unit.unitData.spriteAnimations.currentAnimation.IsOnEnd())
+            {
+                _unit.unitData.listNextStates.Add(new Golem_Idle(_unit));
+            }
         }
 
         public override SpriteAnimationSpec GetSpriteAnimationSpec()
