9bc203745e0e168bf164fa023b9c90e1df39000e
RBG <roundbeargames@gmail.com>
2021-07-30 16:29:45 +0900

    serialization issue fixed

--- RunnerProject_2020_3/Assets/InfiniteRunnerProject/UserInput/UserInput.cs
+++ RunnerProject_2020_3/Assets/InfiniteRunnerProject/UserInput/UserInput.cs
@@ -22,28 +22,34 @@ namespace RB
 
             try
             {
-                commands.AddCommand(new UserCommand(CommandType.ATTACK_A, mouse.leftButton));
-                commands.AddCommand(new UserCommand(CommandType.ATTACK_B, mouse.rightButton));
+                if (mouse != null)
+                {
+                    commands.AddCommand(new UserCommand(CommandType.ATTACK_A, mouse.leftButton));
+                    commands.AddCommand(new UserCommand(CommandType.ATTACK_B, mouse.rightButton));
+                }
 
-                commands.AddCommand(new UserCommand(CommandType.MOVE_UP, keyboard.wKey));
-                commands.AddCommand(new UserCommand(CommandType.MOVE_DOWN, keyboard.sKey));
-                commands.AddCommand(new UserCommand(CommandType.MOVE_LEFT, keyboard.aKey));
-                commands.AddCommand(new UserCommand(CommandType.MOVE_RIGHT, keyboard.dKey));
+                if (keyboard != null)
+                {
+                    commands.AddCommand(new UserCommand(CommandType.MOVE_UP, keyboard.wKey));
+                    commands.AddCommand(new UserCommand(CommandType.MOVE_DOWN, keyboard.sKey));
+                    commands.AddCommand(new UserCommand(CommandType.MOVE_LEFT, keyboard.aKey));
+                    commands.AddCommand(new UserCommand(CommandType.MOVE_RIGHT, keyboard.dKey));
 
-                commands.AddCommand(new UserCommand(CommandType.JUMP, keyboard.spaceKey));
+                    commands.AddCommand(new UserCommand(CommandType.JUMP, keyboard.spaceKey));
 
-                commands.AddCommand(new UserCommand(CommandType.F4, keyboard.f4Key));
-                commands.AddCommand(new UserCommand(CommandType.F5, keyboard.f5Key));
-                commands.AddCommand(new UserCommand(CommandType.F6, keyboard.f6Key));
-                commands.AddCommand(new UserCommand(CommandType.F7, keyboard.f7Key));
-                commands.AddCommand(new UserCommand(CommandType.F8, keyboard.f8Key));
-                commands.AddCommand(new UserCommand(CommandType.F9, keyboard.f9Key));
-                commands.AddCommand(new UserCommand(CommandType.F10, keyboard.f10Key));
-                commands.AddCommand(new UserCommand(CommandType.F11, keyboard.f11Key));
+                    commands.AddCommand(new UserCommand(CommandType.F4, keyboard.f4Key));
+                    commands.AddCommand(new UserCommand(CommandType.F5, keyboard.f5Key));
+                    commands.AddCommand(new UserCommand(CommandType.F6, keyboard.f6Key));
+                    commands.AddCommand(new UserCommand(CommandType.F7, keyboard.f7Key));
+                    commands.AddCommand(new UserCommand(CommandType.F8, keyboard.f8Key));
+                    commands.AddCommand(new UserCommand(CommandType.F9, keyboard.f9Key));
+                    commands.AddCommand(new UserCommand(CommandType.F10, keyboard.f10Key));
+                    commands.AddCommand(new UserCommand(CommandType.F11, keyboard.f11Key));
+                }
             }
-            catch(System.Exception e)
+            catch (System.Exception e)
             {
-                Debugger.Log("weird bug: " + e);
+                Debugger.Log(e);
             }
         }
 
