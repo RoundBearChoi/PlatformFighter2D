833b51f6fb6e34cc1cdc5da0103789121fbd1c13
RBG <roundbeargames@gmail.com>
2021-08-21 11:24:27 +0900

    run transition as statecomponent

--- RunnerProject_2020_3/Assets/InfiniteRunnerProject/LittleRedFighter/LittleRed_Idle.cs
+++ RunnerProject_2020_3/Assets/InfiniteRunnerProject/LittleRedFighter/LittleRed_Idle.cs
@@ -13,6 +13,7 @@ namespace RB
             _listStateComponents.Add(new LerpHorizontalSpeed_FlatGround(ownerUnit, 0f, BaseInitializer.current.fighterDataSO.IdleSlowDownLerpPercentage));
             _listStateComponents.Add(new UpdateDirectionOnInput(ownerUnit));
             _listStateComponents.Add(new TriggerJumpUp(ownerUnit));
+            _listStateComponents.Add(new TriggerRunOnGround(ownerUnit));
             _listStateComponents.Add(new TriggerLittleRedAttackA(ownerUnit));
             _listStateComponents.Add(new TriggerFallState(ownerUnit));
 
@@ -33,19 +34,19 @@ namespace RB
             }
 
             //jump or move
-            if (ownerUnit.unitData.collisionStays.IsTouchingGround(CollisionType.BOTTOM) ||
-                ownerUnit.unitData.collisionEnters.IsTouchingGround(CollisionType.BOTTOM))
-            {
-                if (ownerUnit.USER_INPUT.commands.ContainsHoldOrPress(CommandType.MOVE_LEFT))
-                {
-                    ownerUnit.unitData.listNextStates.Add(new LittleRed_Run(ownerUnit));
-                }
-
-                if (ownerUnit.USER_INPUT.commands.ContainsHoldOrPress(CommandType.MOVE_RIGHT))
-                {
-                    ownerUnit.unitData.listNextStates.Add(new LittleRed_Run(ownerUnit));
-                }
-            }
+            //if (ownerUnit.unitData.collisionStays.IsTouchingGround(CollisionType.BOTTOM) ||
+            //    ownerUnit.unitData.collisionEnters.IsTouchingGround(CollisionType.BOTTOM))
+            //{
+            //    if (ownerUnit.USER_INPUT.commands.ContainsHoldOrPress(CommandType.MOVE_LEFT))
+            //    {
+            //        ownerUnit.unitData.listNextStates.Add(new LittleRed_Run(ownerUnit));
+            //    }
+            //
+            //    if (ownerUnit.USER_INPUT.commands.ContainsHoldOrPress(CommandType.MOVE_RIGHT))
+            //    {
+            //        ownerUnit.unitData.listNextStates.Add(new LittleRed_Run(ownerUnit));
+            //    }
+            //}
         }
     }
 }
