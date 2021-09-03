2dbc7e5bd25be4d89aea3160538918be3f1cbbf8
RBG <roundbeargames@gmail.com>
2021-09-03 14:59:16 +0900

    save

--- PlatformerProject_2020_3/Assets/InfiniteRunnerProject/ResourceLoader/UIType.cs
+++ PlatformerProject_2020_3/Assets/InfiniteRunnerProject/ResourceLoader/UIType.cs
@@ -6,21 +6,14 @@ namespace RB
 {
     public enum UIType
     {
-        //RUNNER_GAME_UI,
-        
+        COMPATIBLE_BASE_UI,
 
+        RUNNER_INFO_LAYER,
+        INTRO_SELECTION_LAYER,
+        
+        //temp
         UI,
         DEFAULT_UI_BLOCK,
         RUNNER_DEATH_NOTIFICATION,
-
-        COMPATIBLE_BASE_UI,
-
-        //FIGHTER_INTRO_UI,
-
-        INTRO_SELECTION_LAYER,
-        RUNNER_INFO_LAYER,
-        //HOST_GAME_UI,
-
-        
     }
 }
