0be33391c6afd2d10bf3deb51bc4538c982d0eee
RBG <roundbeargames@gmail.com>
2021-09-05 15:29:48 +0900

    enterip text select

--- PlatformerProject_2020_3/Assets/InfiniteRunnerProject/EnterHostIP/EnterHostIP.cs
+++ PlatformerProject_2020_3/Assets/InfiniteRunnerProject/EnterHostIP/EnterHostIP.cs
@@ -1,11 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace RB
 {
     public class EnterHostIP : UIElement
     {
+        [SerializeField]
+        InputField _inputField = null;
 
+        public override void InitElement()
+        {
+            StartCoroutine(_setDefaultText());
+        }
+
+        IEnumerator _setDefaultText()
+        {
+            _inputField = this.gameObject.GetComponentInChildren<InputField>();
+
+            yield return new WaitForEndOfFrame();
+
+            _inputField.text = "127.0.0.1";
+            _inputField.ActivateInputField();
+
+            yield return new WaitForEndOfFrame();
+
+            _inputField.MoveTextEnd(false);
+        }
     }
 }
