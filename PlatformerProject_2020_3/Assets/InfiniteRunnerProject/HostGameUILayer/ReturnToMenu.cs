dfa8c87642aad878b99ddfbdf9ed5b6179b37f9a
RBG <roundbeargames@gmail.com>
2021-09-14 11:09:32 +0900

    cancel server on returntomenu

--- PlatformerProject_2020_3/Assets/InfiniteRunnerProject/HostGameUILayer/ReturnToMenu.cs
+++ PlatformerProject_2020_3/Assets/InfiniteRunnerProject/HostGameUILayer/ReturnToMenu.cs
@@ -8,7 +8,14 @@ namespace RB
     {
         public override void OnEnterKey()
         {
+            Debugger.Log("returning to menu");
             BaseInitializer.current.stageTransitioner.AddNextStage(BaseStage.InstantiateNewStage(StageType.INTRO_STAGE));
+            
+            if (RB.Server.NetworkControl.CURRENT != null)
+            {
+                RB.Server.NetworkControl.CURRENT.server.Stop();
+                Destroy(RB.Server.NetworkControl.CURRENT.gameObject);
+            }
         }
     }
 }
