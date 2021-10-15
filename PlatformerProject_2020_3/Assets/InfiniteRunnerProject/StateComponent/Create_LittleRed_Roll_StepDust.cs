881f2d15d44d7d8acca3bd036541cdaf38d4043a
RBG <roundbeargames@gmail.com>
2021-10-15 18:23:07 +0900

    save

--- PlatformerProject_2020_3/Assets/InfiniteRunnerProject/StateComponent/Create_LittleRed_Roll_StepDust.cs
+++ PlatformerProject_2020_3/Assets/InfiniteRunnerProject/StateComponent/Create_LittleRed_Roll_StepDust.cs
@@ -13,18 +13,23 @@ namespace RB
 
         public override void OnFixedUpdate()
         {
-            uint fixedUpdateCount = _unit.iStateController.GetCurrentState().fixedUpdateCount;
-
-            if (fixedUpdateCount != 0 && fixedUpdateCount % _unit.unitData.spriteAnimations.GetCurrentAnimation().SPRITE_INTERVAL == 0)
+            if (!_unit.isDummy)
             {
-                if (_unit.unitData.spriteAnimations.GetCurrentAnimation().SPRITE_INDEX == 1 ||
-                    _unit.unitData.spriteAnimations.GetCurrentAnimation().SPRITE_INDEX == 3 ||
-                    _unit.unitData.spriteAnimations.GetCurrentAnimation().SPRITE_INDEX == 5)
+                SpriteAnimation ani = _unit.unitData.spriteAnimations.GetCurrentAnimation();
+
+                if (ani != null)
                 {
-                    if (!_unit.isDummy)
+                    if (ani.SPRITE_INDEX == 1 ||
+                        ani.SPRITE_INDEX == 3 ||
+                        ani.SPRITE_INDEX == 5)
                     {
-                        BaseMessage showStepDust = new Message_ShowStepDust(false, _unit.transform.position - new Vector3(_unit.transform.right.x * 0.025f, 0f, 0f), new Vector2(1f, 1f), 4);
-                        showStepDust.Register();
+                        uint fixedUpdateCount = _unit.iStateController.GetCurrentState().fixedUpdateCount;
+
+                        if (fixedUpdateCount != 0 && fixedUpdateCount % ani.SPRITE_INTERVAL == 0)
+                        {
+                            BaseMessage showStepDust = new Message_ShowStepDust(false, _unit.transform.position - new Vector3(_unit.transform.right.x * 0.025f, 0f, 0f), new Vector2(1f, 1f), 4);
+                            showStepDust.Register();
+                        }
                     }
                 }
             }
